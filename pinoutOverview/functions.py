00fc541c9865e3f51875a79ec93a736deaffb211
coburnw <rcw@peak.org>
2023-07-29 18:31:33 -0700

    added pin classes

--- pinoutOverview/functions.py
+++ pinoutOverview/functions.py
@@ -7,7 +7,7 @@ else:
     from pinoutOverview import shapes
 
 class Label(dw.Group):
-    def __init__(self, style, template=None, id=None):
+    def __init__(self, style=None, template=None, id=None):
         if template is not None:
             self.set_template(template)
             return
@@ -36,14 +36,33 @@ class Label(dw.Group):
     @property
     def spacing(self):
         return self.template['spacing']
+
+    @property
+    def vert_spacing(self):
+        return self.template['vert_spacing']
         
     @property
     def line_style(self):
         return self.template['label_line_style']
-        
+         
+    @property
+    def skip(self):
+        return "skip" in self.style and self.style['skip']
+
+    @property
+    def slant_left(self):
+        return -1
+
+    @property
+    def slant_right(self):
+        return +1
+
+    @property
+    def slant_none(self):
+        return 0
+    
     def box_generate(self, slant, is_alt):
-        # slant = 1,-1,0 for left, right, or square
-        
+
         style = dict(self.template['box_style'])
         if is_alt:
             style = dict(self.template['alt_box_style'])
@@ -96,37 +115,31 @@ class Label(dw.Group):
 
         return self
 
-class FunctionLabel():
-    def __init__(self, function, id=None, types=None, ):
-        # types are the template function styles available to be placed in a row
-        if types is not None:
-            self.set_types(types)
+class Function():
+    def __init__(self, function, type_templates=None):
+        if type_templates is not None:
+            self.set_types(type_templates)
             return
-        
-        # function is the variant function to be labeled and placed in a row
-        self.function = function
-        self.name = function['name']
-        self.style = self.types[function['type']]
 
-        self.label = Label(style=self.style)
+        self.function = function
         return
-        
+    
     @classmethod
     def set_types(cls, types):
         cls.types = types
         return
 
     @property
-    def height(self):
-        return self.label.height
-    
-    @property
-    def width(self):
-        return self.label.width
+    def name(self):
+        return self.function['name']    
 
     @property
-    def spacing(self):
-        return self.label.spacing
+    def type(self):
+        return self.function['type']
+
+    @property
+    def style(self):
+        return self.types[self.type]
 
     @property
     def is_alt(self):
@@ -134,46 +147,169 @@ class FunctionLabel():
 
     @property
     def skip(self):
-        return "skip" in self.style and self.style['skip']
+        style = self.types[self.function['type']]
+        return "skip" in style and style['skip']
 
     @property
-    def spacer(self):
+    def is_spacer(self):
         return self.function['type'] == 'spacer'
     
-    def generate(self, slant=0):
-        return (self.label.generate(self.name, slant=slant, is_alt=self.is_alt))
+    @property
+    def spacing(self):
+        return Label().spacing
+
+    def generate(self, slant):
+        print('    generating functionlabel: {}'.format(self.name))
+        
+        label = Label(style=self.style)
+        return label.generate(self.name, slant=slant, is_alt=self.is_alt)
     
-class FunctionRow():
-    def __init__(self, id=None, direction=1):
-        self.id = id
+class Functions(dw.Group):
+    def __init__(self, row, direction, id=None):
+        super().__init__()
+
+        self.row = row
         self.direction = direction
 
         self.x = 0
         self.y = 0
-        self.height = 0
-        self.line_style = None
-
-        self.dw_labels = dw.Group()
         return
+        
+    @property
+    def height(self):
+        return Label().height
+    
+    @property
+    def width(self):
+        return abs(self.x) #+ Label().width
 
-    def append(self, label):
-        if self.x == 0:
-            self.x += (label.width/2) * self.direction
-            self.height = label.height
-            self.line_style = label.line_style
-        else:
+    @property
+    def line_style(self):
+        return Label().line_style
+
+    def generate(self, slant=0):
+        print('  generating function row')
+        dw_row = dw.Group() 
+        self.x = Label().width/2 * -self.direction
+        
+        for func in self.row:
+            function = Function(func)
+            print('   appending function {}'.format(function.name))
+            label = function.generate(slant)
+
             self.x += (label.width + label.spacing) * self.direction
+            dw_row.append(dw.Use(label, self.x, self.y))
+
+        self.append(dw.Line(0,0, self.width*self.direction,0, **self.line_style))
+        self.append(dw_row)
         
-        self.dw_labels.append(dw.Use(label, self.x, self.y))
+        return self
+
+# a group of function label rows for a specific pin
+class Pin(dw.Group):
+    def __init__(self, rows):
+        super().__init__()
+        self.rows = rows
         return
 
-    def generate(self):
-        dw_row = dw.Group(id=self.id)
+    @property
+    def label_spacing(self):
+        return Label().spacing
+    
+    @property
+    def row_spacing(self):
+        return Label().height + Label().vert_spacing
         
-        dw_row.append(dw.Line(0,0, self.x, 0, **self.line_style))
-        dw_row.append(self.dw_labels)
+    @property
+    def height(self):
+        count = len(self.rows)
+        return self.spacing * count 
+
+    @property
+    def width(self):
+        return 0
+    
+    def generate(self, x_direction, y_direction, slant=0):
+        print('generating pin with {} rows'.format(len(self.rows)))
+
+        x_start = 0
+        y_start = 0
+         
+        row_count = len(self.rows)
+        if row_count > 4:
+            raise 'four function rows per pin maximum'
         
-        return dw_row
+        if row_count == 1:
+            pass
+        elif row_count % 2 == 0:
+            y_start = (self.row_spacing * row_count/2 - self.row_spacing/2) * -y_direction
+        else:
+            y_start = self.row_spacing * row_count/3 * -y_direction
+
+        x = x_start
+        y = y_start
+
+        i = 0
+        for r in self.rows:
+            row_labels = Functions(r, x_direction)
+            row = row_labels.generate(slant=slant)
+            
+            offset = i * self.row_spacing * y_direction
+            self.append(dw.Use(row, x,y+offset))
+            i += 1
+            
+        if row_count > 1:
+            self.append(dw.Line(0,y_start, 0,y+offset, **row.line_style))
+
+        return self
+
+class Pins():
+    def __init__(self, names, rows):
+        self.names = names   # each name can be a name or a list of names that point to a row in rows
+        self.rows = rows 
+        self.spacing = self.calc_spacing(names)
+        return
+
+    def __len__(self):
+        return len(self.names)
+    
+    def __getitem__(self, i):
+        names = self.names[i]
+        rows = []
+        
+        if hasattr(names, "__len__") and not isinstance(names, str):
+            for name in names:
+                rows.append(self.rows[name])
+        else:
+            name = names
+            rows.append(self.rows[name])
+                
+        return Pin(rows)
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.names):
+            next = self.__getitem__(self.index)
+            self.index += 1
+            return next
+        
+        raise StopIteration
+
+    def calc_spacing(self, names):
+        max_lines = 1
+        
+        for i, m in enumerate(names):
+                if hasattr( m, "__len__" ) and not isinstance( m, str ):
+                    if len(m) > max_lines:
+                        max_lines = len(m)
+
+        pin_spacing = max_lines * (Label().height + Label().vert_spacing)
+
+        return pin_spacing
+
     
 if __name__ == '__main__':
     import os
@@ -210,6 +346,9 @@ if __name__ == '__main__':
     variant['mapping'] = data['mapping']
     variant['pins'] = data['pins']
 
+    #variant['mapping'][2] = ['P5','P6']
+    #variant['mapping'][3] = ['P7','P8']
+    #variant['mapping'][4] = ['P7', 'P9', 'P10']
     pprint.pprint(variant)
 
     x_max = 1500
@@ -218,27 +357,24 @@ if __name__ == '__main__':
     dw_page.embed_google_font("Roboto Mono")
 
     Label(style=None, template=template['label'])
-    FunctionLabel(function=None, types=template['types'])
-
+    Function(function=None, type_templates=template['types'])
+    pins = Pins(names=variant['mapping'], rows=variant['pins'])
+    
     # svg coordinates: +x to the right, +y to the bottom
-    x_direction = 1  # +1 right, -1 left
+    x_direction = -1  # +1 right, -1 left
     y_direction = 1   # +1 down
     x = -600 * x_direction
     y = -400 * y_direction
     
     dw_page.append(dw.Line(x_max,y, -x_max,y, stroke='black'))
     dw_page.append(dw.Line(x,y_max, x,-y_max, stroke='red'))
-    
-    rows = dw.Group()
-    for name in variant['mapping']:
-        pin = variant['pins'][name]
-        row = FunctionRow(id='Pin_'+name, direction=x_direction)
-        for function in pin:
-            label = FunctionLabel(function)
-            row.append(label.generate())
 
-        rows.append(dw.Use(row.generate(), x, y))
-        y += row.height * 1.5 * y_direction 
+    dw_pins = dw.Group()
+    for pin in pins:
+        dw_pin = pin.generate(x_direction, y_direction, slant=Label().slant_none)
+        dw_pins.append(dw.Use(dw_pin, x,y))
+        dw_pins.append(dw.Circle(x,y, 2, stroke='black'))
+        y += pins.spacing * y_direction 
 
-    dw_page.append(rows)
+    dw_page.append(dw_pins)
     dw_page.save_svg('junk.svg')
